edde0c528138fe3313298738ff20ba38fe56c6ba
THE-HERALD\dmunt <dmuntrasi@gmail.com>
2019-01-26 13:34:44 -0600

    Crab AI now correctly pursues its target and stops when nearby. Excitement!

--- Assets/Scripts/CrabController.cs
+++ Assets/Scripts/CrabController.cs
@@ -11,6 +11,9 @@ public class CrabController : MonoBehaviour {
     Transform target;
 
     [SerializeField]
+    bool closeToTarget = false;
+
+    [SerializeField]
     int state = 0;
 
     bool keepUpright = true;
@@ -35,18 +38,28 @@ public class CrabController : MonoBehaviour {
         if (CheckGrounded() == true) keepUpright = true;
         else keepUpright = false;
 
+        if (target != null) closeToTarget = CloseToTarget();
+
         switch (state)
         {
             case (int)State.DO_NOTHING:
-                if (target != null && CloseToTarget() == false) state = (int)State.MOVE;
+                if (target != null && closeToTarget == false) state = (int)State.MOVE;
                 break;
             case (int)State.UPRIGHT:
                 break;
             case (int)State.MOVE:
-                if (CheckUpright() == true && CheckGrounded() == true) MoveTowardsTarget();
+                if (target != null)
+                {
+                    if (closeToTarget == true) Debug.Log(closeToTarget);
+                    if (CheckUpright() == true && CheckGrounded() == true && closeToTarget == false) MoveTowardsTarget();
 
-                // Check if sponge or mother
-                if (CloseToTarget() == true) state = (int)State.DO_NOTHING;
+                    // Check if sponge or mother
+                    else
+                    {
+                        state = (int)State.DO_NOTHING;
+                    }
+                }
+                else state = (int)State.DO_NOTHING;
                 break;
             case (int)State.GATHER:
                 break;
@@ -73,7 +86,7 @@ public class CrabController : MonoBehaviour {
 
     bool CheckUpright()
     {
-        Debug.Log(transform.up.y);
+        //Debug.Log(transform.up.y);
         if (transform.up.y < 0.75f) return false;
         else return true;
     }
@@ -81,17 +94,18 @@ public class CrabController : MonoBehaviour {
     bool CheckGrounded()
     {
         int layerMask = (1 << 8);
+        layerMask |= (1 << 9);
         layerMask |= (1 << 10);
         layerMask = ~(layerMask);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.5f, layerMask);
         if (hit.collider != null && hit.collider.tag == "Ground")
         {
-            Debug.Log("Grounded!");
+            //Debug.Log("Grounded!");
             return true;
         }
         else if (hit.collider != null)
         {
-            Debug.Log(hit.collider.name);
+            //Debug.Log(hit.collider.name);
             return false;
         }
 
@@ -100,9 +114,9 @@ public class CrabController : MonoBehaviour {
 
     bool CloseToTarget()
     {
-        Vector2 dirToTarget = -(transform.position - target.position);
-        Debug.Log(dirToTarget.magnitude);
-        if (dirToTarget.magnitude > 0.5f) return false;
+        float disToTarget = Mathf.Abs(transform.position.x - target.position.x);
+        //Debug.Log(dirToTarget.magnitude);
+        if (disToTarget > 0.5f) return false;
         else return true;
 
     }
