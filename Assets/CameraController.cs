e2a0e4ecdc279ff930098a5cad42429701bfcadb
THE-HERALD\dmunt <dmuntrasi@gmail.com>
2019-01-26 18:29:51 -0600

    Tweaked ResourceSponge settings; added mouse controls

---
+++ Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera cam;
+    public float panSpeed;
+    public float zoomSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentZoom = cam.orthographicSize;
+        if (Input.mousePosition.x > Screen.width - 30)
+            cam.transform.Translate(Vector3.right * 0.1f * panSpeed);
+        else if (Input.mousePosition.x < 30)
+            cam.transform.Translate(Vector3.left * 0.1f * panSpeed);
+        var zoom = Input.GetAxis("Mouse ScrollWheel");
+        if (zoom != 0)
+        {
+            currentZoom -= zoom;
+        }
+        if (currentZoom > 4f && currentZoom < 15f) cam.orthographicSize = currentZoom;
+    }
+}
