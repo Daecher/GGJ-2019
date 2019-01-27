d9361214588231b010ad00b2bdfc0f35af84bfdb
THE-HERALD\dmunt <dmuntrasi@gmail.com>
2019-01-27 14:26:53 -0600

    Adding Scene transition functionality

---
+++ Assets/sceneTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour
+{
+    public string loadScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t")) Load();
+    }
+
+    public void Load()
+    {
+        Debug.Log("HURR");
+        SceneManager.LoadScene(loadScene);
+    }
+}
